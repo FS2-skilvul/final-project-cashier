97bb0d4c662f5b10d73b8425963380706f0e3871
marifsiddiqi <marifsiddiqi81@gmail.com>
2023-11-23 19:17:37 +0700

    refactor: setting database dan env

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const rootRoutes = require('./routes')
 
 const PORT = process.env.PORT || 3000
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(rootRoutes)
 
