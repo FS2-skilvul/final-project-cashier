16153f4b6de68e7c0bdf0cc48ef2d6619c675d9f
marifsiddiqi <marifsiddiqi81@gmail.com>
2023-11-27 19:58:38 +0700

    fix: route dan filenya

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
-import UserDashboard from "./pages/user-dashboard"
-import AdminDashboard from "./pages/admin-dashboard"
 import { Route, Routes } from 'react-router-dom';
+import AdminDashboardPage from "./pages/admin-dashboard"
 import LandingHome from '../src/pages/landing/home';
 import LandingFitur from '../src/pages/landing/fitur';
 import LandingAbout from '../src/pages/landing/about';
-import DashboardPage from './pages/dashboard';
+import UserDashboardPage from './pages/user-dashboard';
 import GudangPage from './pages/gudang';
 import KasirPage from './pages/kasir';
 import Login from './pages/login';
@@ -20,13 +19,12 @@ function App() {
           
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
-          
-        <Route path="/user-dashboard" element={<UserDashboard/>} />
-        <Route path="/admin-dashboard" element={<AdminDashboard/>} />
 
-				<Route path="/dashboard" element={<DashboardPage />} />
+				<Route path="/user-dashboard" element={<UserDashboardPage />} />
 				<Route path="/gudang" element={<GudangPage />} />
 				<Route path="/kasir" element={<KasirPage />} />
+
+        <Route path="/admin-dashboard" element={<AdminDashboardPage/>} />
 			</Routes>
 		</>
 	);
